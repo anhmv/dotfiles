9c346277e65871200139b0e5ae9009ca6b86b6c0
Anh Mac <anhmv91@gmail.com>
2020-07-04 19:15:20 +0700

    add emacs config

---
+++ emacs/init.el
@@ -0,0 +1,43 @@
+;;; init.el --- Emacs init file
+;;; Author: Anh Mac <anhmv91@gmail.com>
+;;;
+;;; A lightweight Emacs config containing only the essentials
+
+
+;; Packages config
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+
+;; Setting up the package manager
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t))
+
+
+;; Load config file "./config.org"
+(require 'org)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(provide 'init)
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (doom-themes use-package powerline evil dracula-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
