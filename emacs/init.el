bff11337e7b9065385b54aaca60f15c959ac65a5
Anh Mac <anhmv91@gmail.com>
2020-07-05 12:24:28 +0700

    update emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -34,10 +34,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (doom-themes use-package powerline evil dracula-theme))))
+    (sublime-themes vue-mode use-package projectile powerline neotree highlight-numbers highlight-escape-sequences evil-magit evil-collection doom-themes all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:background nil)))))
